e053cd613640e44e1783270c9bf22571eceee860
Leon Lentz <leon.lentz@unirez.de>
2023-08-22 09:36:50 +0200

    v0.1.3: Models added!

---
+++ Sandbox/Server/Models/A.cs
@@ -0,0 +1,16 @@
+﻿
+namespace Sandbox.Server.Models
+{
+    internal class A
+    {
+        public int Id { get; set; }
+        public string a1 { get; set; }
+        public string a2 { get; set; }
+        public string a3 { get; set; }
+
+        public string a4 { get; set; }
+        public string a5 { get; set; }
+           
+        public B bObj { get; set; }
+    }
+}
---
+++ Sandbox/Server/Models/B.cs
@@ -0,0 +1,17 @@
+﻿namespace Sandbox.Server.Models
+{
+    public class B
+    {
+        public int Id { get; set; }
+        public string b1 { get; set; }
+        public string b2 { get; set; }
+        public string b3 { get; set; }
+
+        public string b4 { get; set; }
+        public string b5 { get; set; }
+
+        public ICollection<C> CCol { get; set; }
+
+        public ICollection<E> ECol { get; set; }
+    }
+}
